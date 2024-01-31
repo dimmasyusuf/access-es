2f84a8ab00fc5ed3138c16032190738643c597a1
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-01 01:06:38 +0700

    style(Hero): create hero ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import YoutubeVideo from '@/components/YoutubeVideo';
 
 export default async function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center px-0 md:px-8">
       <Hero />
       <Reason />
       <YoutubeVideo />
