546996e8aea9cc0d5cf9adbcbaaea9bf2064c5e7
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-01 12:38:55 +0700

    style(YoutubeVideo): change padding and size for 21:9 aspect ratio

--- components/YoutubeVideo.tsx
+++ components/YoutubeVideo.tsx
@@ -1,13 +1,13 @@
 export default function YoutubeVideo() {
   return (
-    <section className="flex items-center justify-center max-w-screen-xl w-full px-6 sm:px-8 lg:px-16">
-      <div className="flex items-center justify-center py-8 w-full">
+    <section className="flex items-center justify-center max-w-screen-xl w-full px-6 sm:px-8 md:px-0">
+      <div className="flex items-center justify-center py-10 w-full">
         <iframe
           src="https://www.youtube.com/embed/iUXHaf1TRIo?si=kR_JQaFXAkesixrt"
           title="Access English School Profile Video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share fullscreen"
           loading="lazy"
-          className="w-full max-h-[494px] aspect-square rounded-xl border-none"
+          className="w-full max-h-[549px] aspect-square rounded-xl border-none"
         />
       </div>
     </section>
