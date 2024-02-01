c5f7156c5a9d5a621a49ffc7c7a72147e860f60e
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-01 12:43:43 +0700

    style(Reason): add padding vertical 10 at larger than md device

--- components/Reason.tsx
+++ components/Reason.tsx
@@ -17,7 +17,7 @@ import {
 export default function Reason() {
   return (
     <section className="flex px-6 sm:px-8 md:px-0 max-w-screen-xl">
-      <div className="flex flex-col py-8 gap-10 md:gap-16">
+      <div className="flex flex-col py-8 md:py-10 gap-10 md:gap-16">
         <h2 className="font-medium text-3xl md:text-5xl">Kenapa ACCESS-ES?</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
           <div className="flex flex-col gap-4 md:gap-6">
