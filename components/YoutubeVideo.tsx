b8a8c0079136fd2bf0c6f6d70048d306caa99a10
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-01 12:44:43 +0700

    style(YoutubeVideo): add padding vertical 8 at smaller than md device

--- components/YoutubeVideo.tsx
+++ components/YoutubeVideo.tsx
@@ -1,7 +1,7 @@
 export default function YoutubeVideo() {
   return (
     <section className="flex items-center justify-center max-w-screen-xl w-full px-6 sm:px-8 md:px-0">
-      <div className="flex items-center justify-center py-10 w-full">
+      <div className="flex items-center justify-center py-8 md:py-10 w-full">
         <iframe
           src="https://www.youtube.com/embed/iUXHaf1TRIo?si=kR_JQaFXAkesixrt"
           title="Access English School Profile Video"
